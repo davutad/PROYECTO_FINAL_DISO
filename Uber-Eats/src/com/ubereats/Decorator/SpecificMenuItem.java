7adf282061b4be630d3d900a0ffc5ceef4241cfc
davutad <david.san@live.u-tad.com>
2026-05-04 22:23:33 +0200

    Clase SpecificMenuItem

---
+++ Uber-Eats/src/com/ubereats/Decorator/SpecificMenuItem.java
@@ -0,0 +1,34 @@
+
+package com.ubereats
+
+        jkdasf
+public class SpecificMenuItem implements MenuItemComponent{
+    private MenuItem item;
+
+    public SpecificMenuItem(MenuItem item){
+        this.item =item;
+    }
+
+    @Override
+    public String getName(){
+        return item.getName();
+    }
+
+    @Override
+    public double getPrice(){
+        return item.getPrice();
+    }
+
+    @Override
+    public String getDescription(){
+        return item.getDescription();
+    }
+
+    @Override
+    public void print(){
+        System.out.println("===" + getName() + "===");
+        System.out.println("Description: " + getDescription());
+        System.out.println("Price: " + getPrice());
+    }
+
+}
