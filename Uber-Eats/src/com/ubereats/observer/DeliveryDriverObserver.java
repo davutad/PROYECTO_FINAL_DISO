0fc0bcc44d3631a7473d1b097b9b0a7b74e09a63
davutad <david.san@live.u-tad.com>
2026-05-10 12:51:29 +0200

    cambios de siso

--- Uber-Eats/src/com/ubereats/observer/DeliveryDriverObserver.java
+++ Uber-Eats/src/com/ubereats/observer/DeliveryDriverObserver.java
@@ -5,26 +5,28 @@ import com.ubereats.Order;
 import com.ubereats.state.CancelledState;
 import com.ubereats.state.DeliveredState;
 
-public class DeliveryDriverObserver implements OrderObserver{
-	
+public class DeliveryDriverObserver implements OrderObserver {
+
 	private DeliveryDriver deliveryDriver;
-	
+
 	public DeliveryDriverObserver(DeliveryDriver deliveryDriver) {
 		this.deliveryDriver = deliveryDriver;
 	}
- 
+
 	@Override
 	public void update(Order order) {
-		System.out.println("Delivery driver " + deliveryDriver.getUsername() 
-        + "el pedido #" + order.getId() + " del cliente " + order.getClient().getUsername() + "ha cambiado a " 
-		+ order.getOrderState());
-		
-	//Aquí empezamos con los cambios de la lista de pedidos, en caso de entregado o cancelado, se saca de la lista
+		System.out.println("Delivery driver " + deliveryDriver.getUsername()
+				+ " el pedido #" + order.getId() + " del cliente " + order.getClient().getUsername() + " a cambiado a "
+				+ order.getOrderState());
+
+		// Aquí empezamos con los cambios de la lista de pedidos, en caso de entregado o
+		// cancelado, se saca de la lista
 
 		if (order.getOrderState() instanceof DeliveredState || order.getOrderState() instanceof CancelledState) {
-            deliveryDriver.removeOrder(order);
-            deliveryDriver.decrementAssignedOrders(); //Cuando el pedido se termina tambien se decrementa el numero de pedidos que tiene
-    	}
+			deliveryDriver.removeOrder(order);
+			deliveryDriver.decrementAssignedOrders(); // Cuando el pedido se termina tambien se decrementa el numero de
+														// pedidos que tiene
+		}
 
 	}
 
--- Uber-Eats/src/com/ubereats/observer/RestaurantObserver.java
+++ Uber-Eats/src/com/ubereats/observer/RestaurantObserver.java
@@ -5,25 +5,25 @@ import com.ubereats.Restaurant;
 import com.ubereats.state.OnDeliveryState;
 import com.ubereats.state.CancelledState;
 
-public class RestaurantObserver implements OrderObserver{
-	//Restaurante al que pertenece el observador
+public class RestaurantObserver implements OrderObserver {
+	// Restaurante al que pertenece el observador
 	private Restaurant restaurant;
-	
-	//Constructor
+
+	// Constructor
 	public RestaurantObserver(Restaurant restaurant) {
 		this.restaurant = restaurant;
 	}
 
-	//Metodo para decir al restaurante de que ha cambiado el estado del pedido
+	// Metodo para decir al restaurante de que ha cambiado el estado del pedido
 	@Override
 	public void update(Order order) {
 		System.out.println("Restaurante: " + restaurant.getUsername() + " el pedido #" + order.getId()
-		+ " del cliente " + order.getClient().getUsername() + " se ha actualizado a: " 
-		+ order.getOrderState());
-		
+				+ " del cliente " + order.getClient().getUsername() + " se ha actualizado a: "
+				+ order.getOrderState());
+
 		if (order.getOrderState() instanceof OnDeliveryState || order.getOrderState() instanceof CancelledState) {
-            restaurant.removeOrder(order);
-        }
+			restaurant.removeOrder(order);
+		}
 
 	}
 }
