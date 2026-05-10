29b12a75030c655dbde11f98b1b737749c2c783d
orzon98 <daloval8590@gmail.com>
2026-05-10 11:56:45 +0200

    Cambios en deliveryDriver
    He añadido un atributo para contabilizar el numero de pedidos que tiene un driver

--- Uber-Eats/src/com/ubereats/DeliveryDriver.java
+++ Uber-Eats/src/com/ubereats/DeliveryDriver.java
@@ -1,11 +1,28 @@
 package com.ubereats;
 
 public class DeliveryDriver extends User {
-
-    // TODO atribs especicificas de delivery driver
+	//Atributo para tener el numero de pedidos que tiene un driver
+	private Integer assignOrders;
 
     public DeliveryDriver(String username) {
         super(username);
+        this.assignOrders = 0;
+    }
+    
+    public int getAssignedOrders() {
+    	return assignOrders;
+    }
+    
+    //Incrementar el numero de pedidos que tiene el driver
+    public void incrementAssignedOrders() {
+    	this.assignOrders++;
+    }
+    
+    //Decrementar el numero de pedidos que tiene el driver
+    public void decrementAssignedOrders() {
+    	if(assignOrders > 0) {
+    		this.assignOrders--;
+    	}
     }
     
     @Override
